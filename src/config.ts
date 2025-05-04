aea334c954844372c683db6fee6d99d5851a16f1
sazankaa <satorobi9280@gmail.com>
2025-05-04 15:32:33 +0900

    demo -> pic

--- src/config.ts
+++ src/config.ts
@@ -15,8 +15,9 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // 常に表示するか否か
 	},
 	banner: {
-		enable: false,//trueならトップバナーの下に画像を表示可
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,//trueならトップバナーの下に画像を表示可
+		src: "assets/images/aobuta.JPG", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		// src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // trueで画像の注釈を追加可能
@@ -52,7 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/sailor.jpg",
+	// avatar: "assets/images/kaho.jpg",
+	// avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Minatogawa",//プロフィール部分のあれこれ
 	bio: "ペットボトルの芯で筋肉痛。",
 	links: [
