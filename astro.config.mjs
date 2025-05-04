eba01bba0c5759d1ac81f831ba2ba4e5e4733950
sazankaa <satorobi9280@gmail.com>
2025-05-04 18:13:48 +0900

    actions 設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://sazankaa.github.io/Thanks-For-saicaca",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
