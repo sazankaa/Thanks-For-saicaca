9cd2cf9d07524f0d6f1e81fe5ba2dc30b2055f0b
sazankaa <satorobi9280@gmail.com>
2025-05-04 14:02:57 +0900

    config.tsの設定と見た目の確認

--- src/config.ts
+++ src/config.ts
@@ -7,26 +7,26 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	title: "湊川のお部屋",
+	subtitle: "thanks for saicaca",
+	lang: "ja", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		hue: 145, // トップバナーのカラーテーマ変更部分のデフォルト値
+		fixed: false, // 常に表示するか否か
 	},
 	banner: {
-		enable: false,
+		enable: false,//trueならトップバナーの下に画像を表示可
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: false, // trueで画像の注釈を追加可能
+			text: "this is demo-banner.png", // Credit text to be displayed
+			url: "https://baseball.yahoo.co.jp/npb", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
-		enable: true, // Display the table of contents on the right side of the post
-		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+		enable: true, // マークダウン記事の右側に目次の表示
+		depth: 2, // 記事の目次の深さの設定 どのレベルの見出しまでだすか
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
@@ -45,33 +45,33 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
+			url: "https://github.com/sazankaa", // Internal links should not include the base path, as it is automatically added
+			external: true, // リンクであることを示すアイコンを表示するか選択
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Minatogawa",//プロフィール部分のあれこれ
+	bio: "ペットボトルの芯で筋肉痛。",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			name: "Discord",
+			icon: "fa6-brands:discord", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://discord.com/channels/@me",
 		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
+		// {
+		// 	name: "Steam",
+		// 	icon: "fa6-brands:steam",
+		// 	url: "https://store.steampowered.com",
+		// },
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/sazankaa",
 		},
 	],
 };
