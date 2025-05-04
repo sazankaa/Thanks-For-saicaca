ff6b807371f50cd44cef9794502e1c6913930bf2
sazankaa <satorobi9280@gmail.com>
2025-05-04 18:18:57 +0900

    base edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sazankaa.github.io/Thanks-For-saicaca",
-	base: "/",
+	base: "/Thanks-For-saicaca",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
